be1c229d8b3f9bface6615fa31277f497cfb87c9
orzechovaty <orzechovaty@gmail.com>
2017-05-03 15:03:34 +0200

    S-boxy

--- src/Szyfrowanie.java
+++ src/Szyfrowanie.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -37,6 +39,62 @@ public class Szyfrowanie {
 			45, 41, 49, 35, 28, 31
 	};
 	
+	byte[] pR={
+			31, 0, 1, 2, 3, 4, 3, 4, 5, 6, 7, 8,
+			7, 8, 9, 10, 11, 12, 11, 12, 13, 14, 15, 16,
+			15, 16, 17, 18, 19, 20, 19, 20, 21, 22, 23, 24,
+			23, 24, 25, 26, 27, 28, 27, 28, 29, 30, 31, 0
+	};
+	
+	byte[] S1={
+			14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7,
+			0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8,
+			4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0,
+			15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13
+	};
+	byte[] S2={
+			15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10,
+			3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5,
+			0, 14, 7, 11, 10, 4, 13, 1, 5, 7, 12, 6, 9, 3, 2, 15,
+			13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9
+	};
+	byte[] S3={
+			10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8,
+			13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1,
+			13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7,
+			1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12
+	};
+	byte[] S4={
+			7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15,
+			13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9,
+			10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4,
+			3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14
+	};
+	byte[] S5={
+			2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9,
+			14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6,
+			4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14,
+			11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3
+	};
+	byte[] S6={
+			12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11,
+			10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8,
+			9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6,
+			4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13
+	};
+	byte[] S7={
+			4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1,
+			13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6,
+			1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2,
+			6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12
+	};
+	byte[] S8={
+			13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7,
+			1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2,
+			7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8,
+			2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11
+	};
+	
 	String[][] lewa = new String[4][8];
 	String[][] prawa = new String[4][8];
 	String bit56 = "";
@@ -47,6 +105,8 @@ public class Szyfrowanie {
 	String kodBinarnie;
 	String klucz;
 	String kluczBinarnie;
+	
+	List<byte[]> Sbox = new ArrayList<byte[]>();
 	
 	public Szyfrowanie(){
 		String zgoda="y";
@@ -73,6 +133,15 @@ public class Szyfrowanie {
 			}
 		}
 		
+		Sbox.add(S1);
+		Sbox.add(S2);
+		Sbox.add(S3);
+		Sbox.add(S4);
+		Sbox.add(S5);
+		Sbox.add(S6);
+		Sbox.add(S7);
+		Sbox.add(S8);
+		
 		kluczBinarnie = hexToBinary(klucz);
 		pPC1();
 		//System.out.println(lewyKlucz+" "+prawyKlucz);
@@ -80,8 +149,10 @@ public class Szyfrowanie {
 			przesuñBity(i);
 			//System.out.println(lewyKlucz+" "+prawyKlucz);
 			String bit48 = pPC2();
-			
-			//teraz fejstel lol
+			String rozszerzonaPrawa = pRozszerzaj¹ca();
+			String xorowany = XOR(rozszerzonaPrawa, bit48);
+			String nowaPrawa = sBoxy(xorowany);
+			System.out.println(nowaPrawa);
 		}
 		sc.close();
 		
@@ -249,5 +320,110 @@ public class Szyfrowanie {
 		}
 		return wynik;
 		
+	}
+	
+	
+	//permutacja rozszerzaj¹ca
+	public String pRozszerzaj¹ca(){
+		String wynik="";
+		String pom="";
+		for(int i=0; i<4; i++){
+			for(int j=0; j<8; j++){
+				pom+=prawa[i][j];
+			}
+		}
+		for(int i=0; i<48; i++){
+			wynik+=pom.charAt(pR[i]);
+		}
+		
+		return wynik;
+	}
+	
+	//xorowanie
+	public String XOR(String rPrawa, String klucz48){
+		String wynik="";
+		for(int i=0; i<klucz48.length(); i++){
+			if(((rPrawa.charAt(i)+"").equals("1") && (klucz48.charAt(i)+"").equals("1")) ||
+					((rPrawa.charAt(i)+"").equals("0") && (klucz48.charAt(i)+"").equals("0"))){
+				wynik+="0";
+			}else{
+				wynik+="1";
+			}
+		}
+		
+		return wynik;
+	}
+	
+	//SBOXy
+	public String sBoxy(String dane){
+		String wynik="";
+		String[] tab6bit = new String[8];
+		//dzielenie na 6-bitowe ci¹gi
+		int x=0;
+		int y=6;
+		int licznik=0;
+		while(licznik<=7){
+			tab6bit[licznik]=dane.substring(x, y);
+			x=y;
+			y+=6;
+			licznik++;
+		}
+		
+		//lecimy do boxów
+		for(int i=0; i<8; i++){
+			String wiersz = tab6bit[i].charAt(0)+tab6bit[i].charAt(5)+"";
+			String kolumna = tab6bit[i].substring(1, 5);
+			int index=0;
+			switch(wiersz){				
+			case "01":
+				index=16;
+			case "10":
+				index=32;
+			case "11":
+				index=48;
+				break;
+			}
+			wynik+=Bin(Sbox.get(i)[index+Integer.parseInt(kolumna, 2)]);
+		}
+		
+		return wynik;
+	}
+	
+	public String Bin(byte a){
+		switch(a){
+		case 10:
+			return "1010";
+		case 11:
+			return "1011";
+		case 12:
+			return "1100";
+		case 13:
+			return "1101";
+		case 14:
+			return "1110";
+		case 15:
+			return "1111";
+		case 0:
+			return "0000";
+		case 1:
+			return "0001";
+		case 2:
+			return "0010";
+		case 3:
+			return "0011";
+		case 4:
+			return "0100";
+		case 5:
+			return "0101";
+		case 6:
+			return "0110";
+		case 7:
+			return "0111";
+		case 8:
+			return "1000";
+		case 9:
+			return "1001";
+			default: return null;
+		}
 	}
 }
